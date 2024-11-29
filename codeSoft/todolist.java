99cb0f8e4b487eb0a785aef54d649af9ed8b4379
soumadeep sarkar <soumadeepsarkar85@gmail.com>
2024-11-29 20:26:35 +0530

    new changes

--- codeSoft/todolist.java
+++ codeSoft/todolist.java
@@ -1,6 +1,50 @@
-public class todolist {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class todolist {
     public static void main(String[] args) {
-        
+        ArrayList<String> tasks = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Add Task");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Remove Task");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter task: ");
+                    String task = scanner.next();
+                    tasks.add(task);
+                    System.out.println("Task added successfully!");
+                    break;
+                case 2:
+                    System.out.println("\nYour Tasks:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                    break;
+                case 3:
+                    System.out.print("Enter task number to remove: ");
+                    int taskNumber = scanner.nextInt();
+                    if (taskNumber > 0 && taskNumber <= tasks.size()) {
+                        tasks.remove(taskNumber - 1);
+                        System.out.println("Task removed successfully!");
+                    } else {
+                        System.out.println("Invalid task number.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
     }
 }
+    
